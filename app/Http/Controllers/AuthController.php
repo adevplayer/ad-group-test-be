59b8f3778f6ae6ebe8fd10ff49e1817d3cd8d5d2
johanssazores <johanss.zip@gmail.com>
2023-05-28 18:13:30 +0800

    feat: login route functionality with tokens

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -17,6 +19,23 @@ class AuthController extends Controller
 			'ip_label' => $request->input(key: 'ip_label'),
 			'password' => Hash::make($request->input(key: 'password')),
 		]);
+	}
+
+	public function login(Request $request)
+	{
+		if (!Auth::attempt($request->only('email', 'password'))) {
+			return response([
+				'message' => 'Invalid credentials!'
+			], Response::HTTP_UNAUTHORIZED);
+		}
+
+		$user = Auth::user();
+		$token = $user->createToken('token')->plainTextToken;
+		$cookie = cookie('jwt', $token, 60 * 24); // 24 hours
+
+		return response([
+			'message' => 'Success!'
+		])->withCookie($cookie);
 	}
 
 	public function user()
